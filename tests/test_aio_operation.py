2997ea4c7a5a25e2519cb47031e11bd52118c650
Dmitry Orlov <me@mosquito.su>
2020-01-26 13:37:06 +0300

    done

--- tests/test_aio_operation.py
+++ tests/test_aio_operation.py
@@ -4,9 +4,8 @@ import os
 from linux_aio._aio import Context, Operation
 
 
-def test_aio_operation(tmp_path):
-    ctx = Context(16)
-
+async def perform_operations(ctx: Context, *ops: Operation):
+    futures = []
     loop = asyncio.get_event_loop()
 
     def on_read():
@@ -15,67 +14,56 @@ def test_aio_operation(tmp_path):
 
     loop.add_reader(ctx.fileno, on_read)
 
+    for op in ops:
+        f = loop.create_future()
+        op.set_callback(f.set_result)
+        futures.append(f)
+
+    ctx.submit(*ops)
+
+    result = await asyncio.gather(*futures)
+
+    loop.remove_reader(ctx.fileno)
+
+    return result
+
+
+def test_aio_operation(tmp_path):
+    loop = asyncio.get_event_loop()
+    ctx = Context(16)
+
     async def run():
         with open(os.path.join(tmp_path, "temp.bin"), "wb+") as fp:
             fd = fp.fileno()
-            futures = []
             ops = []
 
             for _ in range(32):
-                op = Operation.write(b"Hello world", fd, 0)
-                f = loop.create_future()
-                op.set_callback(f.set_result)
+                ops.append(Operation.write(b"Hello world", fd, 0))
 
-                futures.append(f)
-                ops.append(op)
+            result = await perform_operations(ctx, *ops)
+            assert result == [11] * 32
 
-            ctx.submit(*ops)
-            await asyncio.gather(*futures)
+            op = Operation.fdsync(fd)
+            assert await perform_operations(ctx, op) == [0]
 
-            # op = Operation.fdsync(fd)
-            # assert op.submit(ctx, efd1) == 1
-            #
-            # assert (await results.get())[0] == efd1.fileno
-            #
-            # assert fp.read() == b"Hello world"
-            # fp.seek(0)
-            #
-            # op1 = AIOOperation.write(b"Hello from ", fd, 0)
-            # op2 = AIOOperation.write(b"async world", fd, 11)
-            # op1.prepare(efd1)
-            # op2.prepare(efd2)
-            #
-            # assert ctx.submit(op1, op2) == 2
-            #
-            # res = [
-            #     (await results.get())[0],
-            #     (await results.get())[0],
-            # ]
-            #
-            # assert sorted(res) == sorted([efd1.fileno, efd2.fileno])
-            #
-            # op = AIOOperation.fdsync(fd)
-            # assert op.submit(ctx, efd1) == 1
-            #
-            # assert (await results.get())[0] == efd1.fileno
-            #
-            # assert fp.read() == b"Hello from async world"
-            #
-            # assert op1.eventfd == efd1
-            # assert op2.eventfd == efd2
-            #
-            # assert op1.context == op2.context == ctx
-            #
-            # op = AIOOperation.read(255, fd, 0)
-            # op.submit(ctx, efd1)
-            #
-            # res = await results.get()
-            # assert res[0] == efd1.fileno
-            #
-            # ctx.process_events()
-            #
-            # assert op.get_value() == b"Hello from async world"
+            assert fp.read() == b"Hello world"
+            fp.seek(0)
 
-    for _ in range(10000):
-        loop.run_until_complete(run())
-        # loop.run_until_complete(asyncio.wait_for(run(), 1))
+            op1 = Operation.write(b"Hello from ", fd, 0)
+            op2 = Operation.write(b"async world", fd, 11)
+
+            assert await perform_operations(ctx, op1, op2) == [11, 11]
+
+            assert op1.context == ctx and op2.context == ctx
+
+            op = Operation.fdsync(fd)
+            assert await perform_operations(ctx, op) == [0]
+
+            assert fp.read() == b"Hello from async world"
+
+            op = Operation.read(255, fd, 0)
+            await perform_operations(ctx, op)
+
+            assert op.get_value() == b"Hello from async world"
+
+    loop.run_until_complete(asyncio.wait_for(run(), 5))
