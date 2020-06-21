2b466e527e671f3100bdf8e4b0d9ab3f96ba87dc
Dmitry Orlov <me@mosquito.su>
2020-06-21 22:54:39 +0300

    multiple passes

--- benchmark/benchmark_write_common.py
+++ benchmark/benchmark_write_common.py
@@ -7,20 +7,31 @@ from tempfile import NamedTemporaryFile
 from caio.asyncio_base import AsyncioContextBase
 
 
-data = os.urandom(65534)
+data = os.urandom(1024)
 
 
 async def main(context_maker: typing.Type[AsyncioContextBase]):
     async with context_maker() as context:
         with NamedTemporaryFile(mode="wb+") as fp:
 
-            timer = - time.monotonic()
-            fileno = fp.file.fileno()
+            async def writer(offset = 0):
+                timer = - time.monotonic()
+                fileno = fp.file.fileno()
 
-            futures = []
-            for i in range(1, 2 ** 15):
-                futures.append(context.write(data, fileno, i * len(data)))
+                futures = []
+                for i in range(1, 2 ** 15):
+                    futures.append(
+                        context.write(data, fileno, offset * i * len(data))
+                    )
 
-            await asyncio.gather(*futures)
-            timer += time.monotonic()
-            print('Done', timer)
+                await asyncio.gather(*futures)
+                timer += time.monotonic()
+                print('Done', timer)
+
+                return timer
+
+            timers = []
+            for i in range(10):
+                timers.append(await writer(i))
+
+            print(sum(timers) / len(timers))
