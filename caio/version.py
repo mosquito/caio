963c82d8ca5332537be9efe1b863154d2c6a119a
Dmitry Orlov <me@mosquito.su>
2020-06-21 19:03:23 +0300

    bump

--- caio/version.py
+++ caio/version.py
@@ -5,7 +5,7 @@ package_license = "Apache Software License"
 
 team_email = author_info[0][1]
 
-version_info = (0, 3, 0)
+version_info = (0, 3, 1)
 
 __author__ = ", ".join("{} <{}>".format(*info) for info in author_info)
 __version__ = ".".join(map(str, version_info))
