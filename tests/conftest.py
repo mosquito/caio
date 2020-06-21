5e8b9958709807d8ec905b73321d0c3691e88019
Dmitry Orlov <me@mosquito.su>
2020-06-22 01:55:33 +0300

    use variants for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,13 @@
 import pytest
 
-from caio import variants, variants_asyncio
+from caio import variants, variants_asyncio, Context, AsyncioContext
 
 
-@pytest.fixture(params=variants)
+@pytest.fixture(params=variants + (Context,))
 def context_maker(request):
     return request.param.Context
 
 
-@pytest.fixture(params=variants_asyncio)
+@pytest.fixture(params=variants_asyncio + (AsyncioContext,))
 def async_context_maker(request):
     return request.param.AsyncioContext
