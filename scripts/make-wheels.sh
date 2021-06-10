d550d69f6a078ceeb2f812be4f458838a6cc6f70
Dmitry Orlov <me@mosquito.su>
2021-06-10 14:30:59 +0300

    fix linux wheels

--- scripts/make-wheels.sh
+++ scripts/make-wheels.sh
@@ -1,19 +1,18 @@
 set -ex
 
-SRC=/app/src
-DST=/app/dst
+mkdir -p dist
 
 function build_wheel() {
-	/opt/python/$1/bin/pip wheel ${SRC} -f ${SRC} -w ${DST}
+	/opt/python/$1/bin/pip wheel . -f . -w dist
 }
-
 
 build_wheel cp36-cp36m
 build_wheel cp37-cp37m
 build_wheel cp38-cp38
 build_wheel cp39-cp39
 
-cd ${DST}
-for f in ./*linux_*; do if [ -f $f ]; then auditwheel repair $f -w . ; rm $f; fi; done
+cd dist
+for f in ./*linux_*;
+do if [ -f $f ]; then auditwheel repair $f -w . ; rm $f; fi;
+done
 cd -
-
