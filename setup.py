bbbe88c925e95c09fef981d21b33c3e31fb9d56e
Mosquito <me@mosquito.su>
2023-03-06 10:09:55 +0300

    use aiomisc-pytest module

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ setup(
     python_requires=">=3.7, <4",
     extras_require={
         "develop": [
-            "aiomisc",
+            "aiomisc-pytest",
             "pytest",
             "pytest-cov",
         ],
